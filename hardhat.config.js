a4a64dae2c0cee523a31c632d8364bc7a9fe8de0
Alex <jmotibabua@DESKTOP-ANLPEQD>
2023-02-15 19:26:46 +0400

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("dotenv").config()
+require("hardhat-deploy")
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        goerli: {
+            chainId: 5,
+            blockConfirmations: 6,
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    solidity: "0.8.17",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+}
